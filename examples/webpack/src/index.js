d92a9f2c76a43951978cecc981b216ef6391f314
George Witteman <george@neonpay.com>
2023-12-15 19:04:15 +0000

    example

---
+++ examples/webpack/src/index.js
@@ -0,0 +1,7 @@
+import { loadNeonJs } from "@neonpay/js";
+
+(async () => {
+  const neon = await loadNeonJs("environment-id")
+  console.log(neon)
+  neon.startEmbeddedCheckout({ checkoutId: "4849f34d-966c-4d56-9015-122aca7f9f1b", email: "foo@neonpay.com", phoneNumber: "1234567890" }).mount("#root")
+})();
