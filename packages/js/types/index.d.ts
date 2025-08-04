33bf27fb3ce05b9eaf0befacb02e68063307c0fd
Elangeni Yabba <41975353+Elangeni@users.noreply.github.com>
2025-08-04 15:14:15 -0700

    Add new payments events  (#15)
    We recently created three new payment events:
    1. `payment.started`
    2. `payment.failed`
    3. `payment.switch_country`
    
    We want to updates the types and publish here.

--- packages/js/types/index.d.ts
+++ packages/js/types/index.d.ts
@@ -4,19 +4,38 @@ declare global {
   }
 }
 
+export type CheckoutAmountsUpdatedCallbackData = {
+  itemTotal: number;
+  subtotalAmount: number;
+  taxAmount: number;
+  totalAmount: number;
+};
+export type PurchaseCompletedCallbackData = { checkout: { id: string; purchaseId: string } };
+export type PaymentStartedCallbackData = { checkout: { id: string } };
+export type PaymentFailedCallbackData = {
+  checkout: { id: string };
+  error: { errorMessage: string };
+};
+export type SwitchCountryCallbackData = {
+  checkout: {
+    id: string;
+  };
+  paymentMethodCountry: string | undefined;
+};
+
 export interface Checkout {
   on(event: "ready", callback: () => void): void;
   on(event: "purchase.completed", callback: (data: PurchaseCompletedCallbackData) => void): void;
-  on(
-    event: "checkout.amounts_updated",
-    callback: (data: { itemTotal: number; subtotalAmount: number; taxAmount: number; totalAmount: number }) => void,
-  ): void;
+  on(event: "checkout.amounts_updated", callback: (data: CheckoutAmountsUpdatedCallbackData) => void): void;
+  on(event: "payment.started", callback: (data: PaymentStartedCallbackData) => void): void;
+  on(event: "payment.failed", callback: (data: PaymentFailedCallbackData) => void): void;
+  on(event: "payment.switch_country", callback: (data: SwitchCountryCallbackData) => void): void;
   off(event: "ready", callback: () => void): void;
   off(event: "purchase.completed", callback: (data: PurchaseCompletedCallbackData) => void): void;
-  off(
-    event: "checkout.amounts_updated",
-    callback: (data: { itemTotal: number; subtotalAmount: number; taxAmount: number; totalAmount: number }) => void,
-  ): void;
+  off(event: "checkout.amounts_updated", callback: (data: CheckoutAmountsUpdatedCallbackData) => void): void;
+  off(event: "payment.started", callback: (data: PaymentStartedCallbackData) => void): void;
+  off(event: "payment.failed", callback: (data: PaymentFailedCallbackData) => void): void;
+  off(event: "payment.switch_country", callback: (data: SwitchCountryCallbackData) => void): void;
   mount(domId: string): void;
   unmount(): void;
 }
